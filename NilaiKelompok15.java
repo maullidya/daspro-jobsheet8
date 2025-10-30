2148e32010a0a47b68257a565d7735346a2fb9a2
maullidya <zhramldy@gmail.com>
2025-10-30 14:39:28 +0700

    percobaan 4

---
+++ NilaiKelompok15.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class NilaiKelompok15 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int i, j, nilai;
+       float totalNilai, rataNilai;
+       float rataTertinggi = 0;  
+       int kelompokTertinggi = 0; 
+       i = 1;
+       while (i <= 6) {
+        System.out.println("Kelompok " + i);
+        totalNilai = 0;
+        for(j=1; j<=5; j++){
+            System.out.print("Nilai dari kelompok penilai " + j + " : ");
+            nilai = sc.nextInt();
+            totalNilai += nilai;
+        } 
+        rataNilai = totalNilai / 5;
+        System.out.println("Kelompok " +i+ ": nilai rata-rata = " +rataNilai);
+        if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+        i++;
+
+       }
+       System.out.println("\n====================================");
+       System.out.println("Kelompok dengan rata-rata tertinggi:");
+       System.out.println("Kelompok " + kelompokTertinggi + " dengan nilai rata-rata " + rataTertinggi);
+       System.out.println("====================================");
+      
+    }
+}
