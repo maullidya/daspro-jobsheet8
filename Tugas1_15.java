8fc3b8213608a55663c6b870b7fb012a6009b944
maullidya <zhramldy@gmail.com>
2025-10-30 15:21:38 +0700

    tugas 1

---
+++ Tugas1_15.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Tugas1_15 {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Masukkan N : ");
+        n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            int jumlah = 0;
+
+            System.out.print("n = " + i + " -> jumlah kuadrat = ");
+
+            for (int j = 1; j <= i; j++) {
+                int kuadrat = j * j;
+                jumlah += kuadrat;
+
+                // Menampilkan setiap suku kuadrat
+                System.out.print(kuadrat);
+                if (j < i) {
+                    System.out.print(" + ");
+                }
+            }
+
+            System.out.println(" = " + jumlah);
+        }
+    }
+}
